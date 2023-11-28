6285925df4186d2df8fabfd0d3ebe3e9c8ad6fe6
chenshengmian <2629701836@qq.com>
2023-11-28 10:50:54 +0800

    1128

--- app/page.tsx
+++ app/page.tsx
@@ -19,12 +19,12 @@ export default function HomePage() {
           <span className="text-blue-600">Vercel</span>
         </a>
         <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-gray-300 sm:text-7xl">
-          Generating dream rooms{" "}
+         用AI生成{" "}
           <span className="relative whitespace-nowrap text-blue-600">
             <SquigglyLines />
-            <span className="relative">using AI</span>
+            <span className="relative">你梦想中</span>
           </span>{" "}
-          for everyone.
+          的房间
         </h1>
         <h2 className="mx-auto mt-12 max-w-xl text-lg sm:text-gray-400  text-gray-500 leading-7">
           Take a picture of your room and see how your room looks in different
